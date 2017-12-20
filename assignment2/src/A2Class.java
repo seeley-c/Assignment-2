aa014e1c3e28fe597d4de70881cc53ab9a1eb88b
Callum <32675933+seeley-c@users.noreply.github.com>
2017-12-19 20:19:19 -0600

    Add files via upload
    -Minor fixes
    -commenting

--- assignment2/src/A2Class.java
+++ assignment2/src/A2Class.java
@@ -23,23 +23,23 @@ import javax.swing.JOptionPane;
  	
  	// *********** class constants **********
             
-            private final int MAX = 10;
-            private final int secInMin = 60;
-            private final int distance = 5000;
-            private final double MPStoKMPH = 3.6;
-            private final int OFFSET = 1;
+            private final int MAX = 10;        //constant for max races
+            private final int secInMin = 60;   //constant for seconds in minutes
+            private final int distance = 5000; //constant for distance
+            private final double MPStoKMPH = 3.6; //constant for converting m/s to km/h
+            private final int OFFSET = 1;      //constant for offset
             
             
  	
  	// ********** instance variable **********
  	
-            private int id = 1000;
-            private int min = 0;
-            private int sec = 0;
-            private int Races = 0;
-            private int timeSec[] = new int[MAX];
-            private double speed[] = new double[MAX];
-            private String delim = "[ ]+";
+            private int id = 1000;  //set base id to 1000
+            private int min = 0;    //int for minutes
+            private int sec = 0;    //int for seconds
+            private int Races = 0;  //int for amount of races
+            private int timeSec[] = new int[MAX]; //array for times
+            private double speed[] = new double[MAX]; //array for speeds
+            private String delim = "[ ]+";  //string for delim
             
      
  	// ********** constructors ***********
@@ -48,17 +48,17 @@ import javax.swing.JOptionPane;
             * Purpose:       create a new skater 
             *         
             * Interface:
-            *     in:         none
+            *     in:         int n for id, input for setting races
             *     out:        none
             ********************************************************/ 
             
             public A2Class(int n, String input[]) {
-                id = this.getID(n);
-                System.out.println("Skater: " + id);
+                id = this.getID(n); //set id
+                System.out.println("Skater: " + id); //output skater and id
                 
                 
-                Races = input.length;
-            }
+                Races = input.length; //set races to number of times in input
+            }//end A2Class
             
  	// ********** accessors **********
             
@@ -70,9 +70,9 @@ import javax.swing.JOptionPane;
             *     out:        ID for skater object
             ********************************************************/
             public int getID (int n) {
-                int ID = 1000 + n;
-                return ID;
-            }
+                int ID = 1000 + n;  //set id
+                return ID;  //return id
+            }//end getID
             
             
             /********************************************************
@@ -84,15 +84,15 @@ import javax.swing.JOptionPane;
             ********************************************************/ 
             public int singleTime (String input, int j) {
                 
-                String[] time = input.split("[:]+");
-                min = Integer.parseInt(time[0]);
-                sec = Integer.parseInt(time[1]);
+                String[] time = input.split("[:]+"); //split time
+                min = Integer.parseInt(time[0]); //set minutes
+                sec = Integer.parseInt(time[1]); //set seconds
                 
-                sec = sec + (min * secInMin);
+                sec = sec + (min * secInMin); //set time in seconds
                 
-                timeSec[j] = sec;
-                return sec;
-            }
+                timeSec[j] = sec; //store time in seconds
+                return sec; //return total seconds
+            }//end singleTime
             
             
             /********************************************************
@@ -103,22 +103,33 @@ import javax.swing.JOptionPane;
             *     out:        average time 
             ********************************************************/ 
             public int avgTime() {
-                int avgTime = 0;
-                int totalSec = 0;
+                int avgTime = 0; //int for average time
+                int totalSec = 0; //int for total seconds
+                int min = 0; //int for minutes
+                int sec = 0; //int for seconds
                 
-                for (int n = 0; n < Races; n++) {
-                    totalSec += timeSec[n];
-                }
+                for (int n = 0; n < Races; n++) { //loop for getting total time
+                    totalSec += timeSec[n]; //add time to total time
+                }//end for loop
                 
-                avgTime = totalSec / Races;
+                avgTime = totalSec / Races; //calculate average time
                 
+                min = avgTime / secInMin; //set average time minutes
+                sec = avgTime % secInMin; //set average time seconds
                 
-                System.out.print("Average time: " + (avgTime/secInMin) 
-                        + ":");
-                System.out.format( (avgTime%secInMin) + "\n");
+                if (sec>=10) { //if less than 10 seconds
+                System.out.print("Average time: "); //output average time
+                System.out.print( min + ":");
+                System.out.print(sec + "\n");
+                }//end if statement
+                else { //more than 9 seconds
+                System.out.print("Average time: "); //output average time
+                System.out.print( min + ":");
+                System.out.print("0" + sec + "\n");
+                }//end else statment
                 
-                return avgTime;
-            }
+                return avgTime; //return average time in seconds
+            }//end avgTime
             
             
             
@@ -131,23 +142,23 @@ import javax.swing.JOptionPane;
             *     out:        average speed (double)
             ********************************************************/ 
             public double avgSpeed() {
-                double avgSpeed = 0;
-                double totalSpeed = 0;
+                double avgSpeed = 0;   //double for average speed
+                double totalSpeed = 0; //double for total speed
                 
                 
                 
-                for (int n = 0; n < Races; n++) {
-                    totalSpeed += speed[n];
-                }
+                for (int n = 0; n < Races; n++) { //loop for getting total speed
+                    totalSpeed += speed[n]; //add speed to total speed
+                }//end for loop
                 
-                avgSpeed = totalSpeed / Races;
+                avgSpeed = totalSpeed / Races; //calculate average speed
                 
-                System.out.print("Average speed: ");
+                System.out.print("Average speed: "); //output average speed
                 System.out.format("%.2f", avgSpeed);
                 System.out.println("Km/h\n");
                 
-                return avgSpeed;
-            }
+                return avgSpeed; //return average speed
+            }//end avgSpeed
             
             
             /********************************************************
@@ -158,72 +169,101 @@ import javax.swing.JOptionPane;
             *     out:        speed of skater in race (double)
             ********************************************************/ 
             public double singleSpeed(double n, int j) {
-                double singleSpeed = 0;
+                double singleSpeed = 0; //double for speed
                 
-                singleSpeed = distance/n * MPStoKMPH;
+                singleSpeed = distance/n * MPStoKMPH; //calculate speed
                 
-                speed[j] = singleSpeed;
+                speed[j] = singleSpeed; //store speed in array
                 
-                return singleSpeed;
-            }
+                return singleSpeed; //return speed
+            }//end singleSpeed
 
             
             
             
  	// ********** mutators **********
             
+            /********************************************************
+            * Purpose:        change or add time
+            *         
+            * Interface:
+            *     in:         none
+            *     out:        time that was added/changed
+            ********************************************************/ 
             public int changeTime(){
-                String chanTo = "";
-                int chanMin = 0;
-                int chanSec = 0;
-                int chanTotSec = 0;
+                String chanTo = ""; //string for time input
+                int chanMin = 0;    //int for changed time minutes
+                int chanSec = 0;    //int for changed time seconds
+                int chanTotSec = 0; //int for changed time total seconds
+                int min = 0;    //int for outputting time in minutes
+                int sec = 0;    //int for outputting time in seconds
                 
                 
                 chanTo = JOptionPane.showInputDialog(
                         "What time do you want to add (ex: 7:15)");
+                //input the time to be added/changed
                 
-                String[] chan = chanTo.split("[:]+");
+                String[] chan = chanTo.split("[:]+"); //split input
                 
-                chanMin = Integer.parseInt(chan[0]);
-                chanSec = Integer.parseInt(chan[1]);
-                chanTotSec = (chanMin * secInMin) + chanSec;
+                chanMin = Integer.parseInt(chan[0]); //set changed time minutes
+                chanSec = Integer.parseInt(chan[1]); //set changed time seconds
+                chanTotSec = (chanMin * secInMin) + chanSec; //set total seconds
                 
-                if (Races < 10){ 
-                    timeSec[Races] = chanTotSec;
+                if (Races < 10){ //if there are less than 10 races
+                    timeSec[Races] = chanTotSec; //add time
                     
-                    min = timeSec[Races] / secInMin;
-                    sec = timeSec[Races] % secInMin;
+                    min = timeSec[Races] / secInMin; //set minutes for output
+                    sec = timeSec[Races] % secInMin; //set seconds for output
                     
                     System.out.print("You have changed race time " 
-                        + (Races + OFFSET) + " to " );
-                    System.out.format("%2s", (timeSec[Races]/secInMin) + ":"); 
-                    System.out.format("%2s", (timeSec[Races]%secInMin) + "\n");
+                        + (Races + OFFSET) + " to " ); //output race number
+                    
+                    
+                    if (sec >= 10) { //if there are more than 9 seconds
+                    System.out.print(min + ":"); 
+                    System.out.print("0" + sec + "\n"); //output time
+                    }//end if statement
+                    else { //less than 10 seconds
+                    System.out.print(min + ":"); 
+                    System.out.print(sec + "\n"); //output time
+                    }//end else
                     
                     System.out.print("Average race " + (Races + OFFSET) + " speed: ");
+                    //output start of race speed
                     
-                    Races = Races + OFFSET;
+                    Races = Races + OFFSET; //set number of races
                 }//end if
                 
                 else {
-                    for (int n = 1; n < Races; n++) {
+                    for (int n = 1; n < Races; n++) { 
+                    //loop for moving race times down one place
                         timeSec[(Races - OFFSET) - n] = timeSec[Races - OFFSET];
-                    }
-                    timeSec[Races - OFFSET] = chanTotSec;
+                        //set race times down one place
+                    }//end for loop
                     
-                    min = timeSec[Races-OFFSET] / secInMin;
-                    sec = timeSec[Races-OFFSET] % secInMin;
+                    timeSec[Races - OFFSET] = chanTotSec;//set changed race time
+                    
+                    min = timeSec[Races-OFFSET] / secInMin; //set changed time minutes
+                    sec = timeSec[Races-OFFSET] % secInMin; //set changed time seconds
                     
                     System.out.print("You have changed race time " 
-                        + (Races) + " to " );
-                    System.out.format("%2s", 
-                            (timeSec[Races-OFFSET]/secInMin) + ":"); 
-                    System.out.format("%2s", 
-                            (timeSec[Races-OFFSET]%secInMin) + "\n");
+                        + (Races) + " to " ); //output race number
                     
-                    System.out.print("Average race " + Races + " speed: ");
+                    if (sec >= 10) { //if there are more than 9 seconds
+                    System.out.print(min + ":"); 
+                    System.out.print("0" + sec + "\n"); //output time
+                    }//end if statement
+                    else { //if there are less than 10 seconds
+                    System.out.print(min + ":"); 
+                    System.out.print(sec + "\n"); //output time
+                    }//end else 
+                    
+                    
+                    System.out.print("Average race " + Races + " speed: "); 
+                    //start output of race speed
                 }//end else
                 
-                return chanTotSec;
-            }
+                return chanTotSec; //return changed time
+            }//end changeTime
  
  }  // end class
--- assignment2/src/A2Client.java
+++ assignment2/src/A2Client.java
@@ -5,9 +5,12 @@
  * Assignment: Assignment 2
  * Program Name:  A2Client.java
  *
- * Description: 
+ * Description: read in times from a text file, create a skater object,
+ *              assign a skater ID, print out all of the skater's race
+ *              times, find the speed of the individual races, find the
+ *              average race speed, find the average race time
  *
- * Input: 
+ * Input:  race times from text file
  *
  * Output: skater id, average time, race times, average speed,
  *         single race speed
@@ -26,26 +29,26 @@ public class A2Client {  // begin class
     
     // ********* declaration of constants **********
     
-        final int MAX = 10;
-        final int OFFSET = 1;
-        final int SENTVAL = -1;
-        final int secInMin = 60;
+        final int MAX = 10;         //constant for max number of races
+        final int OFFSET = 1;       //constant for the offset for changing times
+        final int SENTVAL = -1;     //constant for sentinel value
+        final int secInMin = 60;    //constant for seconds in one minute
     
     // ********** declaration of variables **********
     	
-    	String delim = "[ ]+";		// delimiter string for splitting input string
-        String delim2 = "[;]+";
-        String strin = "";
-        String input[] = null;
-        int n = 0;
-        int timesInt[] = new int[MAX];
-        int averageTime = 0;
-        double singleSpeed = 0;
-        int singleTime = 0;
-        double averageSpeed = 0;
-        int chanTime = 0;
-        int chan = 0;
-        int races = 0;
+    	String delim = "[ ]+";    // delimiter string for splitting input string
+        String delim2 = "[;]+";   //delimiter for splitting race times
+        String strin = "";        //string for input
+        String input[] = null;    //string for splitting inpujt
+        int n = 0;                //int for incrementing id
+        int timesInt[] = new int[MAX];  //int for storing times in seconds
+        int averageTime = 0;      //integer to store average time in seconds
+        double singleSpeed = 0;   //double for storing speed of race
+        int singleTime = 0;       //int for storing time of race in seconds
+        double averageSpeed = 0;  //double for storing average speed
+        int chanTime = 0;         //int for storing changed time in seconds
+        int chan = 0;             //int for checking if a time will be changed
+        int races = 0;            //int for storing number of races
         
         
     	
@@ -57,86 +60,89 @@ public class A2Client {  // begin class
     	BufferedReader fin = new BufferedReader(new FileReader("skaterData.txt"));
     	PrintWriter fout = new PrintWriter(new BufferedWriter(new FileWriter("textOutA2.txt")));
         
-        ProgramInfo programInfo = new ProgramInfo("Assignment 2");
+        ProgramInfo programInfo = new ProgramInfo("Assignment 2"); //create new program info
     	
     // ********** Print output Banner **********
     
-    	System.out.println(programInfo.toString());
+    	System.out.println(programInfo.toString()); //output program info
         //fout.println(programInfo.toString());
         
  	    	
     // ************************ get input **********************
-        
+
+    // ************************ processing ***************************
     
-    
-        strin = fin.readLine();
+        strin = fin.readLine(); //input first line
         
-        while(strin != null){
-            chan = 0;
-            int j = 0;
-            input = strin.split(delim);
+        while(strin != null){ //while there is still input
+            chan = 0;  //reset chan value
+            int j = 0; //reset j value
+            input = strin.split(delim); //split race
             
-            races = input.length;
+            races = input.length; //set number of races
             
-            A2Class skater = new A2Class(n, input);
+            A2Class skater = new A2Class(n, input); //create new skater object
           
             for(int i = 0; i < races; i++){
-                timesInt[n] = skater.singleTime(input[i], j);
+                timesInt[n] = skater.singleTime(input[i], j); 
+                //store race time in seconds
                 singleSpeed = skater.singleSpeed(timesInt[n], j);
+                //store race speed
                 
-                System.out.println("Race " + (i + OFFSET) + " time: " + input[i]);
+                System.out.print("Race " + (i + OFFSET) + " time: ");
+                System.out.format(input[i] + "\n");
+                //output race time
                 System.out.print("Average race " + (i + OFFSET) + " speed: ");
                 System.out.format("%.2f", singleSpeed);
                 System.out.println("Km/h");
-                //System.out.println("Time: " + timesInt[n]);
+                //output race speed
                 
-                
-                j++;
+                j++; //increment int for setting time and speed
             } // end for tokens
             
-            while (chan != SENTVAL) {
+            while (chan != SENTVAL) { //while the user does not enter the sentinel value
                 
                 chan = Integer.parseInt(JOptionPane.showInputDialog(
-                 "Enter any number to change a time for skater " + skater.getID(n) + "\nOr enter -1 to exit"));
+                 "Enter any number to change a time for skater " 
+                  + skater.getID(n) + "\nOr enter -1 to exit"));
+                //input for changing time
                 
-                if (chan == SENTVAL) {
-                    break;
+                if (chan == SENTVAL) { //check if the entered the sentinel value
+                    break;             //break while loop
                 }//end if statement
                 
-                chanTime = skater.changeTime();
+                chanTime = skater.changeTime(); //output and store changed time
                 
-                if (races < MAX) {
+                if (races < MAX) { //if there are less than 10 races
                     singleSpeed = skater.singleSpeed(chanTime, races);
-                }//end if
+                    //output race speed
+                }//end if statement
                 else {
                     singleSpeed = skater.singleSpeed(chanTime, (races - OFFSET));
-                }
+                    //output race speed
+                }//end else statement
                 
-                System.out.format("%.2f", singleSpeed);
-                System.out.println("Km/h");
+                System.out.format("%.2f", singleSpeed + "Km/h"); //output speed
                 } //end while loop
             
             
             
-            averageTime = skater.avgTime();
-            averageSpeed = skater.avgSpeed();
+            averageTime = skater.avgTime();   //output and store average time
+            averageSpeed = skater.avgSpeed(); //output and store average speed
             //System.out.println(averageTime + "\n");
             
           //System.out.println(strin);
-          n++;
-          strin = fin.readLine();
+          n++; //increment n for id
+          strin = fin.readLine(); //read in next line
         } // end eof loop
-    	
-
-    // ************************ processing ***************************
     
     // ************************ print output ****************************
 
     
         // ******** closing message *********
         
-        System.out.println(programInfo.eoFile());
-        fout.println(programInfo.eoFile());
+        System.out.println(programInfo.eoFile()); //output eof statement
+        fout.println(programInfo.eoFile()); //output eof statement to file
         
         // ***** close streams *****
         
